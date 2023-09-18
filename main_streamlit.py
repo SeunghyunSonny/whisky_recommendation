f3fb17f7ee7e734dce0dea4839c6662122e54742
haemii <117583653+haemzz@users.noreply.github.com>
2023-09-18 16:58:48 +0900

    Update main_streamlit.py

--- main_streamlit.py
+++ main_streamlit.py
@@ -36,7 +36,7 @@ if choose == "Contents":
     st.title("나만의 위스키")
 
     # 진한 가로선 추가
-    st.markdown('<hr style="border-top: 3px solid #000;">', unsafe_allow_html=True)
+    # st.markdown('<hr style="border-top: 3px solid #000;">', unsafe_allow_html=True)
 
     # 이미지를 불러옵니다.
     image = Image.open("contents1.png")
@@ -71,13 +71,13 @@ if choose == "Recommendation":
      # 간격 조정
     st.subheader("")
     
-    types = {'whiskey type을 선택해주세요': None,
+    types = {'위스키 타입을 선택해주세요': None,
             '싱글 몰트 위스키': '싱글몰트', 
             '블렌디드 위스키' : '블렌디드', 
             '블렌디드 몰트 위스키' : '블렌디드 몰트',
             '버번 & 라이 위스키' : '버번/라이'}
     
-    selected_type = st.selectbox("**whiskey type 선택**", types.keys())
+    selected_type = st.selectbox("**위스키 타입 선택**", types.keys())
     
     # 간격 조정
     st.subheader("")  
@@ -91,7 +91,7 @@ if choose == "Recommendation":
         min_price = int(min_price)
         max_price = int(max_price)
     # 입력된 최소가격과 최대가격으로 필요한 처리 수행
-        st.write(f"가격 범위는 {int(min_price)}원부터 {int(max_price)}까지 입니다")
+        st.write(f"가격 범위는 {int(min_price)}원부터 {int(max_price)}원 까지 입니다")
 
 
     # 간격 조정
@@ -107,30 +107,30 @@ if choose == "Recommendation":
     st.subheader("")
     
     # 'aroma를 선택' 제목 추가
-    st.write("**aroma 선택**")
+    st.write("**향 선택**")
 
     # 체크 박스를 가로로 8개 나열
     col1_aroma, col2_aroma, col3_aroma, col4_aroma = st.columns(4)
     user_cats_list = []
     # 각 열(column)에 4개씩 체크 박스 추가
     with col1_aroma:
-        option1_aroma = st.checkbox('**나무향**', key="aroma_option1")
+        option1_aroma = st.checkbox('**나무**', key="aroma_option1")
         if option1_aroma:
             user_cats_list.append('aroma_나무향')
-        option2_aroma = st.checkbox('**잔류액향**', key="aroma_option2")
+        option2_aroma = st.checkbox('**잔류액**', key="aroma_option2")
         if option2_aroma:
             user_cats_list.append('aroma_잔류액향')
             
     with col2_aroma:
-        option3_aroma = st.checkbox('**와인향**', key="aroma_option3")
+        option3_aroma = st.checkbox('**와인**', key="aroma_option3")
         if option3_aroma:
             user_cats_list.append('aroma_와인향')
-        option4_aroma = st.checkbox('**피트향**', key="aroma_option4")
+        option4_aroma = st.checkbox('**피트**', key="aroma_option4")
         if option4_aroma:
             user_cats_list.append('aroma_피트향')       
 
     with col3_aroma:
-        option5_aroma = st.checkbox('**곡물향**', key="aroma_option5")
+        option5_aroma = st.checkbox('**곡물**', key="aroma_option5")
         if option5_aroma:
             user_cats_list.append('aroma_곡물향')  
         option6_aroma = st.checkbox('**꽃향기**', key="aroma_option6")
@@ -140,36 +140,36 @@ if choose == "Recommendation":
     with col4_aroma:
         option7_aroma = st.checkbox('**과일향**', key="aroma_option7")
         if option7_aroma:
-            user_cats_list.append('aroma_과일향')  
+            user_cats_list.append('aroma_과일')  
         option8_aroma = st.checkbox('**유황**', key="aroma_option8")
         if option8_aroma:
             user_cats_list.append('aroma_유황')  
 
     # 'taste를 선택' 제목 추가
-    st.write("**taste를 선택**")
+    st.write("**맛 선택**")
 
     # 체크 박스를 가로로 8개 나열
     col1_taste, col2_taste, col3_taste, col4_taste = st.columns(4)
 
     # 각 열(column)에 4개씩 체크 박스 추가
     with col1_taste:
-        option1_taste = st.checkbox('**나무향**', key="taste_option1")
+        option1_taste = st.checkbox('**나무**', key="taste_option1")
         if option1_taste:
             user_cats_list.append('taste_나무향')  
-        option2_taste = st.checkbox('**잔류액향**', key="taste_option2")
+        option2_taste = st.checkbox('**잔류액**', key="taste_option2")
         if option2_taste:
             user_cats_list.append('taste_잔류액향')  
 
     with col2_taste:
-        option3_taste = st.checkbox('**와인향**', key="taste_option3")
+        option3_taste = st.checkbox('**와인**', key="taste_option3")
         if option3_taste:
             user_cats_list.append('taste_와인향')  
-        option4_taste = st.checkbox('**피트향**', key="taste_option4")
+        option4_taste = st.checkbox('**피트**', key="taste_option4")
         if option4_taste:
             user_cats_list.append('taste_피트향')  
 
     with col3_taste:
-        option5_taste = st.checkbox('**곡물향**', key="taste_option5")
+        option5_taste = st.checkbox('**곡물**', key="taste_option5")
         if option5_taste:
             user_cats_list.append('taste_곡물향')  
         option6_taste = st.checkbox('**꽃향기**', key="taste_option6")
@@ -179,36 +179,36 @@ if choose == "Recommendation":
     with col4_taste:
         option7_taste = st.checkbox('**과일향**', key="taste_option7")
         if option7_taste:
-            user_cats_list.append('taste_과일향')  
+            user_cats_list.append('taste_과일')  
         option8_taste = st.checkbox('**유황**', key="taste_option8")
         if option8_taste:
             user_cats_list.append('taste_유황')  
 
     # 'finish를 선택' 제목 추가
-    st.write("**finish를 선택**")
+    st.write("**여운 선택**")
 
     # 체크 박스를 가로로 8개 나열
     col1_finish, col2_finish, col3_finish, col4_finish = st.columns(4)
 
     # 각 열(column)에 4개씩 체크 박스 추가
     with col1_finish:
-        option1_finish = st.checkbox('**나무향**', key="finish_option1")
+        option1_finish = st.checkbox('**나무**', key="finish_option1")
         if option1_finish:
             user_cats_list.append('finish_유황')  
-        option2_finish = st.checkbox('**잔류액향**', key="finish_option2")
+        option2_finish = st.checkbox('**잔류액**', key="finish_option2")
         if option2_finish:
             user_cats_list.append('finish_잔류액향')  
 
     with col2_finish:
-        option3_finish = st.checkbox('**와인향**', key="finish_option3")
+        option3_finish = st.checkbox('**와인**', key="finish_option3")
         if option3_finish:
             user_cats_list.append('finish_와인향')  
-        option4_finish = st.checkbox('**피트향**', key="finish_option4")
+        option4_finish = st.checkbox('**피트**', key="finish_option4")
         if option4_finish:
             user_cats_list.append('finish_피트향')  
 
     with col3_finish:
-        option5_finish = st.checkbox('**곡물향**', key="finish_option5")
+        option5_finish = st.checkbox('**곡물**', key="finish_option5")
         if option5_finish:
             user_cats_list.append('finish_곡물향')  
         option6_finish = st.checkbox('**꽃향기**', key="finish_option6")
@@ -216,7 +216,7 @@ if choose == "Recommendation":
             user_cats_list.append('finish_꽃향기')  
 
     with col4_finish:
-        option7_finish = st.checkbox('**과일향**', key="finish_option7")
+        option7_finish = st.checkbox('**과일**', key="finish_option7")
         if option7_finish:
             user_cats_list.append('finish_과일향')  
         option8_finish = st.checkbox('**유황**', key="finish_option8")
@@ -233,14 +233,13 @@ if choose == "Recommendation":
     preprocessor.preprocessing()
 
     user_name = 'User01' 
-    show_list = ['Whisky Name','price','type','capacity','alcohol','country']
+    show_list = ['위스키 이름','가격','타입','용량','도수','국가']
     
     if (types[selected_type] is not None) & (type(min_price) == int) & (type(max_price) == int) & (len(user_cats_list) > 0):
         Result = True
     else:
         Result = False
 
-    st.write(types[selected_type])
     # "선택 완료" 버튼을 누르면 스피너가 나타나도록 설정
     if st.button("**선택 완료**"):
         with st.spinner("**위스키 추천 중입니다...**"):
@@ -252,7 +251,10 @@ if choose == "Recommendation":
             user_types = [types[selected_type]]
             user_price = (min_price, max_price) # (min_price , max_price)
             similar_shows = preprocessor.find_similar_shows(user_name, user_types, user_price, user_cats_list, top_n=5)
-            st.write(similar_shows[show_list])
+            similar_shows.rename(columns = {'nameKor': '위스키 이름','type':'타입',
+                                'price':'가격','capacity':'용량', 'country':'국가','alcohol':'도수'},inplace = True)
+            
+            st.dataframe(similar_shows[show_list], hide_index=True)
             
         elif Result == False:
             st.write('### :blue[값을 채워주세요!]')
