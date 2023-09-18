953e797840f6832bd682e02f60ef2a9a9bb86ab9
salmmiakki <135793301+salmmiakki@users.noreply.github.com>
2023-09-18 09:33:00 +0900

    Update embeddin.py

--- embeddin.py
+++ embeddin.py
@@ -1,21 +1,27 @@
-import pandas as pd
 from langchain.vectorstores import Chroma
 from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 
+# 간단한 Document 클래스 정의
+class Document:
+    def __init__(self, content):
+        self.page_content = content
+        self.metadata = {}
+
 class DocEmbedding:
 
-    def __init__(self, file_path='./docent/whiskey.txt', openai_api_key="YOUR_ACTUAL_OPENAI_API_KEY", model_name="ada-002"):
+    def __init__(self, txt_file_path=r'.\txt', openai_api_key="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", model_name="ada-002"):
         self.openai_api_key = openai_api_key
         self.model_name = model_name
-        self.documents = self.load_data_from_txt(file_path)
+        self.documents = self.load_data_from_txt(txt_file_path)
         self.docs = self.split_docs()
         self.embeddings = OpenAIEmbeddings(openai_api_key=self.openai_api_key, model_name=self.model_name)
         self.db = self.create_chroma_from_docs()
 
+    # txt 파일을 읽는 메서드
     def load_data_from_txt(self, file_path):
         with open(file_path, 'r', encoding='utf-8') as file:
-            documents = file.readlines()
+            documents = [Document(line.strip()) for line in file.readlines()]
         return documents
 
     def split_docs(self, chunk_size=500, chunk_overlap=40):
@@ -35,8 +41,7 @@ class DocEmbedding:
     def persist_chroma(self, persist_directory="chroma_db"):
         return Chroma.from_documents(documents=self.docs, embedding=self.embeddings, persist_directory=persist_directory)
 
-# Usage
+# 사용 예시
 doc_embed = DocEmbedding()
 query_result = doc_embed.similarity_search("give information about {}")
 print(query_result)
-
