ba855f77ff6b2f379e8e1758577747c56f8cf018
salmmiakki <135793301+salmmiakki@users.noreply.github.com>
2023-09-18 09:44:05 +0900

    Update whiskeylangchain.py

--- whiskeylangchain.py
+++ whiskeylangchain.py
@@ -1,4 +1,5 @@
-from langchain import OpenAI, create_pandas_dataframe_agent
+from langchain import (OpenAI,
+from langchain.agents.agent_toolkits import create_pandas_dataframe_agent
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from embeddin import DocEmbedding
@@ -7,51 +8,57 @@ import pandas as pd
 
 class LangChainWhiskey:
     def __init__(self, api_key, directory="./docent"):
-        self.llm = OpenAI(openai_api_key=api_key)
-        self.doc_embed = DocEmbedding(directory=directory)
-        self.df = self.whiskeydataframe()
+        self.llm = OpenAI(openai_api_key=api_key)  # OpenAI 객체 초기화
+        self.doc_embed = DocEmbedding(directory=directory)  # DocEmbedding 객체 초기화
+        self.df = self.whiskeydataframe()  # 데이터프레임 로드
 
-        # Dynamically get the context based on whiskey
+        # 위스키에 대한 동적 컨텍스트 가져오기
         self.context = self.retrieve_context()
 
-        # WHISKEY template with context using dataframe
+        # 데이터프레임을 사용하여 WHISKEY 템플릿 생성
         self.WHISKEY_template = self.create_template_from_df(
-            "Given the context: {context}. Provide detailed information about the whiskey: {whiskey}."
+            "Given the context: {context}. Using korean language appropriately, Provide detailed information about the whiskey: {whiskey}.",
+            use_last_column=False
         )
 
-        # DOCENT template with context using dataframe
+        # 데이터프레임을 사용해 DOCENT 템플릿 생성
         docent_text = self.create_template_from_df(
             """Given the context: {context}. 
-            Re-write the whiskey given below like a whiskey sommelier of the whiskey bar:
+            Using korean language appropriately, Re-write the whiskey given below like a whiskey sommelier of the whiskey bar:
             whiskey:
             {whiskey}
-            """
+            """,
+            use_last_column=True
         )
         self.DOCENT_template = PromptTemplate(
             input_variables=["whiskey", "context"],
             template=docent_text
         )
 
+    # 데이터프레임 로드 함수
     def whiskeydataframe(self):
         df = pd.read_csv(r'./docent/whisky_preprocessing_done (1).csv', encoding='utf-8')
         return df
 
-    def create_template_from_df(self, base_template):
-        # Sample a random row from dataframe for example
+    # 데이터프레임을 바탕으로 동적 템플릿 생성 함수
+    def create_template_from_df(self, base_template, use_last_column):
+        # 데이터프레임에서 샘플 행 가져오기
         sample_row = self.df.sample(1).iloc[0]
-        example_text = ' '.join(sample_row.values.astype(str))
+        if use_last_column:
+            example_text = sample_row.values[-1]
+        else:
+            example_text = ' '.join(sample_row.values[:-1].astype(str))
         return base_template.replace("{whiskey}", example_text)
 
+    # 동적 컨텍스트 검색 함수
     def retrieve_context(self):
-        # This function uses DocEmbedding to retrieve dynamic context
         context_results = self.doc_embed.similarity_search("context about whiskey")
-
-        # Assuming context_results is a list and we want the most relevant one
         if isinstance(context_results, list) and context_results:
             return context_results[0]
         else:
-            return context_results  # if it's already a string or other format
+            return context_results
 
+    # 위스키 정보 검색 함수
     def get_whiskey_info(self, whiskey_name):
         WHISKEY_chain = LLMChain(
             llm=self.llm,
@@ -61,6 +68,7 @@ class LangChainWhiskey:
         )
         return WHISKEY_chain.run(whiskey=whiskey_name, context=self.context)
 
+    # 도슨트 설명 생성 함수
     def get_docent_description(self, whiskey_name):
         DOCENT_chain = LLMChain(
             llm=self.llm,
